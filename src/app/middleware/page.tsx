797b2bf1b9b563775290d00544acc68540290e9f
Dannick010 <danmaggot2@gmail.com>
2025-04-30 16:24:45 -0300

    feat/prisma

---
+++ src/app/middleware/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        
+        <h2>Pagina protegida por middleware</h2>
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Link href={"/client"}>Pagina com client</Link>
       <Link href={"/server"}>Pagina com server</Link>
+      <Link href={"/middleware"}>Pagina com Middleware</Link>
     </div>
   );
 }
